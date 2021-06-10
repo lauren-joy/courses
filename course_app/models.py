88841907c3dd6e239684174f8fe9325656a8c663
Lauren Babb <laurenjbabb@gmail.com>
2021-06-10 11:57:10 -0500

    update

--- course_app/models.py
+++ course_app/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class CourseManager(models.Manager):
+    def validator(self, postData):
+        errors={}
+        if len(postData['name']) < 5:
+            errors["name"] = "Name must be more than 5 characters"
+        if len(postData['description']) < 15:
+            errors["description"] = "Description must be more than 15 characters"
+        return errors
+
 class Course(models.Model):
     name= models.CharField(max_length=255)
     description= models.CharField(max_length=255)
     created_at= models.DateTimeField(auto_now_add=True)
     updated_at= models.DateTimeField(auto_now=True)
+    objects= CourseManager()
--- course_app/views.py
+++ course_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Course
+from django.contrib import messages
 # Create your views here.
 def index(request):
     context= {
@@ -8,10 +9,16 @@ def index(request):
     return render(request, 'index.html', context)
 
 def add(request):
-    course = Course.objects.create(
-        name= request.POST['name'],
-        description= request.POST['description'],
-    )
+    if request.method == "POST":
+        errors = Course.objects.validator(request.POST)
+    if len(errors) > 0:
+        for value in errors.items():
+            messages.error(request, value)
+    else:
+        course = Course.objects.create(
+            name= request.POST['name'],
+            description= request.POST['description'],
+        )
     return redirect('/')
 
 def destroy(request, course_id):
